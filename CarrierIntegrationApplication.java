b8871391cd677f2f7c5cfde3fa0ffc98bba8546e
Smarthut13 <hutchin.1305@gmail.com>
2026-04-29 22:26:16 +0530

    Initial commit: Implemented carrier integration and shipment workflow backend using Spring Boot, REST APIs, and MySQL

---
+++ CarrierIntegrationApplication.java
@@ -0,0 +1,11 @@
+package com.logistics.carrierintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrierIntegrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarrierIntegrationApplication.class, args);
+    }
+}
