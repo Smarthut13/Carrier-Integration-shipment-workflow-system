34b2479e570150aea32bd99c7db52016bd177833
Smarthut13 <hutchin.1305@gmail.com>
2026-04-29 22:46:25 +0530

    Initial commit: Added Spring Boot backend for carrier integration and shipment workflow system

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO carriers (name, service_level, base_cost, cost_per_kg, estimated_delivery_days, active) VALUES
+('Amazon Shipping', 'TWO_DAY', 90.00, 22.00, 2, true),
+('BlueDart', 'EXPRESS', 140.00, 30.00, 1, true),
+('Delhivery', 'STANDARD', 70.00, 18.00, 4, true),
+('India Post', 'ECONOMY', 45.00, 12.00, 7, true);
